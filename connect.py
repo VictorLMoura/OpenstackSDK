3cf162eed1313a170669b81cdbc4327a2c07ad50
GustavoGB <gustavo.gobetti98@gmail.com>
2019-10-25 11:13:13 -0300

    conec feita, falta criar e deletar as intancia

---
+++ connect.py
@@ -0,0 +1,17 @@
+from openstack import connection
+
+
+conn = connection.Connection(
+    region_name='RegionOne',
+    auth=dict(
+        auth_url='http://192.168.0.27:5000/v3',
+        username='cloud',
+        password='chahWeiteegh9chu',
+        project_id='bf61a57b38be479b9e43321830e7acfb',
+        user_domain_id='admin_domain'),
+    compute_api_version='3',
+    identity_interface='internal')
+
+##Bionic
+
+conn.get_flavor()
