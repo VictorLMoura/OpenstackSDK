5e328b8d8e4bd53edf42d27b4edbeee9fbcc0f3a
GustavoGB <gustavo.gobetti98@gmail.com>
2019-11-01 11:51:22 -0300

    yaml colocado

--- connect.py
+++ connect.py
@@ -1,50 +1,34 @@
 import openstack
-import glance.client.v2.client as glclient
-
+import time
 
 #Initialize  cloud
 conn = openstack.connect(cloud = 'openstack')
 
-for server in conn.compute.server():
-    print(server.to_dict())
 
 
-#Listar imagens
 
+#Find flavor with at least 1024M of region_name
+def list_flavors(conn):
+        print("List Flavors:")
 
-#Find flavor with at least 512M of region_name
+        for flavor in conn.compute.flavors():
+                print(flavor)
 
-flavor = conn.get_flavor_by_ram(512)
-
+flavor = conn.get_flavor_by_ram(1024)
 
 #Boot a server, wait for it to boot.
 
+#Find network
+def list_networks(conn):
+    print("List Networks:")
+
+    for network in conn.network.networks():
+        print(network)
+
 conn.create_server(
     'server', image=image, flavor=bionic,wait= True, auto_ip = True,network = network)
-
 
 conn.delete_server(
     'server')
 
 print("Deletou Instancia")
-
-#conn = connection.Connection(
-#    region_name='RegionOne',
-#    auth=dict(
-#        auth_url='http://192.168.0.27:5000/v3',
-#        username='admin',
-#        password='chahWeiteegh9chu',
-#        project_id='bf61a57b38be479b9e43321830e7acfb',
-#        user_domain_id='admin_domain'),
-#    compute_api_version='3',
-#    identity_interface='internal')
-
-
-
-##Bionic, pegar um hardware especifico
-
-#conn.get_flavor()
-
-#conn.network_find_network()
-
-#conn.create_server()
